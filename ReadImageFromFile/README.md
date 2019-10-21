7a7dfb05c58f454b1706afe3952854a51afa27fd
Mateus Bueno <mateusmobueno@gmail.com>
2019-10-20 22:39:31 -0300

    Read Image sample

---
+++ ReadImageFromFile/README.md
@@ -0,0 +1,20 @@
+# ReadImage
+
+A small program that reads an image and display information of original image
+and in grayscale mode
+
+## How to run
+You can run direct from terminal with no arguments, which will display
+information of 'tokyo.jpg' file
+```bash
+python3 ReadImage.py
+```
+
+Or you can run passing the path of the image file by two ways
+```bash
+python3 ReadImage.py --path /path/to/image
+```
+
+```bash
+python3 ReadImage.py -p /path/to/image
+```
---
+++ ReadImageFromFile/ReadImage.py
@@ -0,0 +1,37 @@
+# Source from Alexey Spizhevoy and Aleksandr Rybnikov OpenCV 3 Computer Vision with Python Cookbook
+
+import argparse
+import cv2
+
+# Create an object for parsing argument from command line
+parser = argparse.ArgumentParser()
+
+# Defines the argument --image and -i for setting image path. Default is 
+# tokyo.jpg which is included in this repository
+parser.add_argument('--path', '-p', default='tokyo.jpg', help='Image path.')
+params = parser.parse_args()
+
+# Read image from path in a NumPy array
+image = cv2.imread(params.path)
+
+# Check wether the image was successfully loaded
+assert image is not None
+
+# Print image path
+print('read {}'.format(params.path))
+
+# Print width, height and color channels
+print('shape: ', image.shape)
+
+# Print undelying image data type
+print('dtype: ', image.dtype)
+
+# Load the image in grayscale mode
+image = cv2.imread(params.path, cv2.IMREAD_GRAYSCALE)
+assert image is not None
+
+print('read {} as grayscale'.format(params.path))
+
+print('shape: ', image.shape)
+
+print('dtype: ', image.dtype)
