20b4f7236eb3a9e260aba84d9daac773d46a0a43
Mateus Bueno <mateusmobueno@gmail.com>
2019-10-23 16:44:44 -0300

    Program to save compressed images in lossy and lossless methods

---
+++ SavingWithCompression/README.md
@@ -0,0 +1,19 @@
+# SavingCompressed
+
+Program to save compressed images in lossy and lossless methods.
+
+## How to run
+You can run direct from terminal with no arguments, which will compress
+and save 'tokyo.jpg' file
+```bash
+python3 SavingCompressed.py
+```
+
+Or you can run passing the path of the image file by two ways
+```bash
+python3 SavingCompressed.py --path /path/to/image
+```
+
+```bash
+python3 SavingCompressed.py -p /path/to/image
+```
---
+++ SavingWithCompression/SavingCompressed.py
@@ -0,0 +1,30 @@
+# Source from Alexey Spizhevoy and Aleksandr Rybnikov OpenCV 3 Computer Vision with Python Cookbook
+
+import argparse
+import cv2
+
+# Create an object for parsing argument from command line
+parser = argparse.ArgumentParser()
+
+# Defines the argument --image and -i for setting image path. Default is 
+# tokyo.jpg which is included in this repository
+parser.add_argument('--path', '-p', default='../res/tokyo.jpg', help='Image path.')
+params = parser.parse_args()
+
+# Read image from path in a NumPy array
+image = cv2.imread(params.path)
+
+# Check wether the image was successfully loaded
+assert image is not None
+
+# Save image with lower compression
+# Bigger file size but faster decoding
+cv2.imwrite('tokyo-compressed.png', image, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+
+# Check that image saved and loaded again is the same as original one
+saved_image = cv2.imread('tokyo-compressed.png')
+assert saved_image.all() == image.all()
+
+# Save image with lower quality
+# Smaller file size
+cv2.imwrite('tokyo-compressed.jpg', image, [cv2.IMWRITE_JPEG_QUALITY, 0])
